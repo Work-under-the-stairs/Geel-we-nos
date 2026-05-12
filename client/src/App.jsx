4ef9113b6a5fa5a874f0d96864eb34ba704d0fa5
Tasneem Alaa <tasnemalaa30@gmail.com>
2026-05-12 22:41:44 +0300

    Add routing and layout components; integrate react-hot-toast for notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import {Toaster} from 'react-hot-toast'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Category from './pages/Category'
+import Layout from './components/layout/Layout'
+import Home from './pages/Home'
+import News from './pages/News'
 
 const App = () => {
+  const userData = true;
   return (
-    <div>App
-      <h1 className="text-3xl font-bold text-red-500">Geel We Nos</h1>
-    </div>
+    <>
+      <Toaster position="top-center" reverseOrder={true} />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/" element={userData? <Layout/> : <Login/>} >
+          <Route index element={<Home/>}/>
+          <Route path="/:category" element={<Category/>}>
+            <Route path=":id" element={<News/>}/>
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   )
 }
 
