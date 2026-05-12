ed6df1b89d038fcb447c908e6bf21979c0dd0fd3
Tasneem Alaa <tasnemalaa30@gmail.com>
2026-05-13 00:20:57 +0300

    Add layout components with user authentication; implement loading state and navbar

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Outlet} from 'react-router-dom'
+import Loading from './Loading'
+import Navbar from './Navbar'
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+
+    const user = true;
+
+    return user ? (
+        <div className='w-full flex flex-col h-screen'>
+            <Navbar />
+            <div className='flex-1 bg-slate-50'>
+                <Outlet />
+            </div>
+        </div>
+    ) : (
+        <Loading height=''/>
+    )
 }
 
 export default Layout
